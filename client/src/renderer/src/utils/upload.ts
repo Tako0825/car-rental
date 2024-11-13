443107bb7b16c87c674a848c56d4b842ce176adc
Tako <2060364922@qq.com>
2024-11-14 05:43:30 +0800

    fix: 生成正确的文件夹名

--- client/src/renderer/src/utils/upload.ts
+++ client/src/renderer/src/utils/upload.ts
@@ -61,8 +61,8 @@ const getUploadToken = async () => {
 const generateKey = () => {
     const date = new Date()
     const year = date.getFullYear()
-    const month = date.getMonth() - 1
-    const day = date.getDay()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
     const key = [year, month, day].join('-') + '/' + v4()
     return key
 }
